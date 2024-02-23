7c03016eeb9ebe66c46f625f0ce4b190f6b7db7d
B0neh3ad <js1044k@gmail.com>
2024-02-23 10:54:45 +0900

    Add nav bar and letter list

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,34 @@
+/* eslint-disable */
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // 자주 바뀌는, 중요한 데이터는 state로
+  // 그래야 새로고침 없이 재렌더링 가능
+  let [편지제목, 편지제목변경] = useState(['안녕 잘 지내니?', '사랑하는 아들에게', 'ㅎㅇㅎㅇ']); 
+  let posts = '제목'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div style={{fontSize: '30px'}}>인편을 쓰자!</div>
+      </div>
+      <div className='list'>
+        <h3> { 편지제목[0] } </h3>
+        <p>2월 17일 전송</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { 편지제목[1] } </h3>
+        <p>2월 18일 전송</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { 편지제목[2] } </h3>
+        <p>2월 29일 전송예약</p>
+        <hr/>
+      </div>
     </div>
   );
 }
