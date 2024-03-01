b9473b5f426ee84fb7cd1fbe081f196c79b60cd5
B0neh3ad <js1044k@gmail.com>
2024-03-01 12:40:34 +0900

    Edit message in DeleteReviewModal

--- hw1/src/components/DeleteReviewModal.tsx
+++ hw1/src/components/DeleteReviewModal.tsx
@@ -1,25 +1,23 @@
+import { useState } from 'react';
 import modalStyles from '../styles/common/Modal.module.css';
 import styles from '../styles/DeleteReviewModal.module.css';
 
 type DeleteReviewModalProps = {
     showModal: boolean,
-    snackName: string,
+    snackName: string | undefined,
     onClose(): void,
     onDelete(): void,
 };
 
 function DeleteReviewModal({ showModal, snackName, onClose, onDelete }: DeleteReviewModalProps) {
-    if (!showModal) {
-        return null;
-    }
     return (
-        <div className={modalStyles.background}>
-            <div className={modalStyles.content} data-testid="write-review-modal">
+        <div className={`${modalStyles.background} ${!showModal ? modalStyles.out : ""}` } onClick={onClose}>
+            <div className={modalStyles.content} data-testid="write-review-modal" onClick={e => e.stopPropagation}>
                 <div className={styles.header}>
                     <h2 className={modalStyles.title}>리뷰 삭제</h2>
                 </div>
                 <div className={styles.body}>
-                    {`"${snackName}"에 대한 리뷰를 삭제하시겠습니까?`}
+                    {snackName !== undefined ? `"${snackName}"에 대한 리뷰를 삭제하시겠습니까?` : "삭제 중..."}
                 </div>
                 <div className={modalStyles.footer}>
                     <button data-testid="delete-review-delete" className={`${modalStyles.button} ${styles.deleteReviewDeleteButton}`} onClick={onDelete}>삭제</button>
