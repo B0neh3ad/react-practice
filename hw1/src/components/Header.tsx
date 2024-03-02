b191c66bdbf1817bcd46792926d5573bfb0795cc
B0neh3ad <js1044k@gmail.com>
2024-03-02 11:15:29 +0900

    Resolve favicon problem both dev and product env

--- hw1/src/components/Header.tsx
+++ hw1/src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header className={styles.header} data-testid="header">
             <a className={styles.logoWrap} href="https://wafflestudio.com">
-                <img className={styles.waffleLogo} src="/public/favicon.ico" alt="wafflestudio logo" data-testid="waffle-logo" />
+                <img className={styles.waffleLogo} src="/favicon.ico" alt="wafflestudio logo" data-testid="waffle-logo" />
                 <h1 className={styles.headerTitle} data-testid="header-title">과자 리뷰</h1>
             </a>
         </header>
