f59e56f3034689fb3577551888b997a53cebc0cb
B0neh3ad <js1044k@gmail.com>
2024-03-05 11:20:37 +0900

    Add SnackItem in SnackDetail page

--- hw1/src/components/SnackList.tsx
+++ hw1/src/components/SnackList.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useSnackContext } from "../contexts/SnackContext";
 import styles from '../styles/SnackList.module.css';
 import SnackItem from "./SnackItem";
@@ -11,7 +12,9 @@ function SnackList() {
                 <ul className={styles.snackWrapper} data-testid="snack-list">
                     {snacks.map(snack => (
                         <li key={snack.id}>
-                            <SnackItem snack={snack} />
+                            <Link to={`./${snack.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                <SnackItem snack={snack} />
+                            </Link>
                         </li>
                     ))}
                 </ul>
--- hw1/src/pages/SnackDetail.tsx
+++ hw1/src/pages/SnackDetail.tsx
@@ -2,9 +2,25 @@
  * 'snacks/:id': 과자 상세 페이지
  */
 
-function SnackDetail() {
+import { useParams } from "react-router-dom";
+import { useSnackContext } from "../contexts/SnackContext";
+import SnackItem from "../components/SnackItem";
+
+function SnackDetail({}) {
+    const { id } = useParams();
+    const { getSnackById } = useSnackContext();
+
+    const snackId = Number(id);
+    const snack = getSnackById(snackId)!;
+
+    if(snack === null){
+        throw new Error("님아;;");
+    }
+
+    
     return (
         <>
+        <SnackItem snack={snack}></SnackItem>
         </>
     );
 }
