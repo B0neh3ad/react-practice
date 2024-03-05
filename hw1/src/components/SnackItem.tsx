f35d231ace4b932ab28d894d753aefdba1b76bff
B0neh3ad <js1044k@gmail.com>
2024-03-05 23:35:40 +0900

    Add css to snackitem to horizontal sort

--- hw1/src/components/SnackItem.tsx
+++ hw1/src/components/SnackItem.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/SnackItem.module.css";
 function SnackItem({ snack }: { snack: Snack }) {
     return (
         <>
-            <div className={styles.snackItem} data-testid="snack-card">
+            <div className={styles.snackItem} data-testid="snack-card" style={{display: "inline-block"}}>
                 <img className={styles.snackImage} data-testid="snack-image" src={snack.image} alt="과자 사진"></img>
                 <div className={styles.snackName}>{snack.snack_name}</div>
                 <div className={styles.rating}>★{snack.rating.toFixed(1)}</div>
